6a128b725eeb4f5e52dc998aa6fd28f0fb770579
Administrator <2353987275@qq.com>
2023-01-02 22:48:23 +0800

    增加

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.util.Log;
 import android.view.View;
 import android.widget.*;
@@ -10,6 +13,10 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "按钮";
     private String imgid;
+    private ProgressBar pb;
+    private Button btnAlert;
+    private Button btnprogress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         EditText edt = (EditText) findViewById(R.id.editText);
         TextView textView = (TextView) findViewById(R.id.text_view);
         ImageView img = (ImageView) findViewById(R.id.imgs);
+        pb = (ProgressBar) findViewById(R.id.progress_bar);
+        btnAlert = (Button) findViewById(R.id.btnAlert);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +45,55 @@ public class MainActivity extends AppCompatActivity {
                     default:
                         break;
                 }
+
+                //显示隐藏进度条
+//                if (pb.getVisibility() == View.GONE) {
+//                    pb.setVisibility(View.VISIBLE);
+//                } else {
+//                    pb.setVisibility(View.GONE);
+//                }
+
+                // 水平进度条
+                int progress = pb.getProgress();
+                progress = progress + 10;
+                pb.setProgress(progress);
             }
         });
+
+        btnAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("警告窗口");
+                dialog.setMessage("一些信息");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d(TAG, "警告对话框按了确认");
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d(TAG, "警告对话框按了取消");
+                    }
+                });
+                dialog.show();
+            }
+        });
+        //无法运行....
+//        btnprogress.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ProgressDialog progressdialog = new ProgressDialog(MainActivity.this);
+//                progressdialog.setTitle("进度条警告框");
+//                progressdialog.setMessage("正在运行...");
+//                progressdialog.setCancelable(true);
+//                progressdialog.show();
+//
+//            }
+//        });
+
     }
 }
